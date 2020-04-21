a145d6a47690b089a759472baa095f95deb0de9d
Tony <doom3.acdc@gmail.com>
2020-04-21 17:43:02 +0300

    deleted needless text

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,27 +5,7 @@ const reducer = (state, action) => {
   return {
     bookList: updateBookList(state, action),
     shoppingCart: updateShoppingCart(state, action)
-  }
-
-  // switch (action.type) {    
-  //   case 'FETCH_BOOKS_REQUEST':
-  //   case 'FETCH_BOOKS_SUCCESS':
-  //   case 'FETCH_BOOKS_FAILURE':
-  //     return {
-  //       ...state,
-  //       bookList: updateBookList(state, action)
-  //     };
-
-  //   case 'BOOK_ADDED_TO_CART':
-  //   case 'BOOK_REMOVED_FROM_CART':
-  //   case 'ALL_BOOKS_REMOVED_FROM_CART':
-  //     return {
-  //       ...state,
-  //       shoppingCart: updateShoppingCart(state, action)
-  //     }
-  //   default:
-  //     return state;
-  
+  };
 };
 
 export default reducer;
