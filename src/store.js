7326e8a9b153a8e94d0c5f6d67bef2e37dae64d0
Tony <doom3.acdc@gmail.com>
2020-04-24 19:13:57 +0300

    Middleware

--- src/store.js
+++ src/store.js
@@ -1,59 +1,58 @@
-import { createStore, compose } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import reducer from './reducers/index';
 
 // const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 // const store = createStore(reducer, composeEnhancers(applyMiddleware(...middleware)
 // ));
 
-const stringEnhancer = (createStore) => (...args) => {
-  const store = createStore(...args);
-  const originalDispatch = store.dispatch;
-  store.dispatch = (action) => {
+// const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && 
+//   window.__REDUX_DEVTOOLS_EXTENSION__());
 
-    if (typeof action === 'string') {
-      return originalDispatch({
-        type: action
-      });
-    }
-
-    return originalDispatch(action);
-  };
-
-  return store;
+const logMiddleware = ({getState, dispatch}) => (next) => (action) => {
+  console.log(action.type, getState())
+  return next(action);
 };
 
-const logEnhancer = (createStore) => (...args) => {
-  const store = createStore(...args);
-  const originalDispatch = store.dispatch;
-  store.dispatch = (action) => {
-    console.log(action.type)
-    return originalDispatch(action);
-  };
-
-  return store;
+const stringMiddleware =() => (next) => (action) => {
+  if (typeof action === 'string') {
+    return next({
+      type: action
+    });
+  }
+  return next(action);
 };
+
+// const logEnhancer = (createStore) => (...args) => {
+//   const store = createStore(...args);
+//   const originalDispatch = store.dispatch;
+//   store.dispatch = (action) => {
+//     console.log(action.type)
+//     return originalDispatch(action);
+//   };
+
+//   return store;
+// };
+
+// const stringEnhancer = (createStore) => (...args) => {
+//   const store = createStore(...args);
+//   const originalDispatch = store.dispatch;
+//   store.dispatch = (action) => {
+
+//     if (typeof action === 'string') {
+//       return originalDispatch({
+//         type: action
+//       });
+//     }
+
+//     return originalDispatch(action);
+//   };
+
+//   return store;
+// };
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, composeEnhancers(stringEnhancer, logEnhancer));
-
-// const store = createStore(reducer, /*window.__REDUX_DEVTOOLS_EXTENSION__ && 
-//   window.__REDUX_DEVTOOLS_EXTENSION__(),*/ logEnhancer);
-// const store = createStore(reducer, 
-//               compose(stringEnhancer, logEnhancer));
-
-
-// Monkey patch
-// const originalDispatch = store.dispatch;
-// store.dispatch = (action) => {
-
-//   if (typeof action === 'string') {
-//     return originalDispatch({
-//       type: action
-//     });
-//   }
-
-//   return originalDispatch(action);
-// };
+const store = createStore(reducer, composeEnhancers(applyMiddleware(
+  stringMiddleware, logMiddleware )));
 
 store.dispatch('HELLO')
 
